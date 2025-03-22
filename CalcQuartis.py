6228ef53cfb9c603e05c1e0050dbf6eee3fa9097
José Roberval Vieira Gomes Neto <64707935+robervalgneto@users.noreply.github.com>
2025-03-22 17:14:40 -0300

    Create CalcQuartis.py

---
+++ CalcQuartis.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+renda_fam = [
+    5, 6.8, 9, 3, 17, 13, 11.2, 5.4, 13, 16, 16, 16.3, 11.7, 12, 4, 3, 9, 13, 12, 9, 2, 1, 3, 4, 2, 
+    8, 1, 19.3, 12.5, 19, 19, 2, 5.5, 5, 2, 7, 2, 3.2, 9.5, 1.5, 5, 18, 6, 9, 2, 1, 16, 10, 9.4, 4.5
+]
+
+renda_fam_sorted = sorted(renda_fam)
+
+Q1 = np.percentile(renda_fam_sorted, 25)
+Q2 = np.percentile(renda_fam_sorted, 50)
+Q3 = np.percentile(renda_fam_sorted, 75)
+
+Q1, Q2, Q3
